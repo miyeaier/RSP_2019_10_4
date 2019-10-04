9fd9831f292368344169db74c45dcdae41787296
miyesier <miyesier@gmail.com>
2019-10-04 14:26:19 +0200

    Finishing format

--- spec.helper.js
+++ spec.helper.js
@@ -2,5 +2,5 @@ const chai = require('chai');
 const BrowserHelpers = require('e2e_training_wheels')
 global.browser = new BrowserHelpers()
 global.expect = chai.expect;
-// global.PC_Choice = require('./src/js/RSP')
 global.RSP = require('./src/js/game_rsp')
+// global.PC_Choice = require('./src/js/RSP')
