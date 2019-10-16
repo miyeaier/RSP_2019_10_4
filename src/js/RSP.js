ac7ed534ec43e504ef1ca4eb5f51afadd0471311
miyesier <miyesier@gmail.com>
2019-10-16 13:45:19 +0200

    Delete old js and spec and clean up code

--- src/js/RSP.js
+++ src/js/RSP.js
@@ -1,51 +1,44 @@
-
 let pc_choice = PC_Choice();
 
 function PC_Choice() {
-  const rand_num = Math.random();  
-     if (rand_num < 0.333) {
-       return "rock";
-     } else if (rand_num<0.666){
-       return "scissors";
-     } else if  (rand_num<1.000){
-       return "paper";
-     }
-}    
-function Compare(user_choice,computer_choice){
-  
-  if(user_choice===computer_choice){
-    return"Tie";
+  const rand_num = Math.random();
+  if (rand_num < 0.333) {
+    return "rock";
+  } else if (rand_num < 0.666) {
+    return "scissors";
+  } else if (rand_num < 1.000) {
+    return "paper";
   }
-    else if(user_choice==="rock"){
-        if(computer_choice==="scissors"){
-    return ("user win");
-         } else{
-      return ("cumputer win")
-         }
 }
-   else if(user_choice==="rock"){
-        if(computer_choice==="paper"){
-    return ("user win");
-        }else{
 
-          return("cumputer win")
-        }
-      
-      }
+function Compare(user_choice, computer_choice) {
 
-      else if(user_choice==="scissors"){
-        if(computer_choice==="paper"){
-    return ("user win");
-         } else{
-
-          return("cumputer wun")
-         }
+  if (user_choice === computer_choice) {
+    return "Tie";
+  } else if (user_choice === "rock") {
+    if (computer_choice === "scissors") {
+      return ("user win");
+    } else {
+      return ("cumputer win")
     }
-   };
-   
-  // console.log(Compare(player_Choice,PC_Choice()));
-  
+  } else if (user_choice === "rock") {
+    if (computer_choice === "paper") {
+      return ("user win");
+    } else {
+
+      return ("cumputer win")
+    }
+
+  } else if (user_choice === "scissors") {
+    if (computer_choice === "paper") {
+      return ("user win");
+    } else {
+
+      return ("cumputer wun")
+    }
+  }
+};
 
 if (typeof module !== 'undefined' && module.exports) {
-  module.exports = PC_Choice,Compare;
-} 
+  module.exports = PC_Choice, Compare;
+}
